2402f62da36307cd8dd3fe96f7ac12c80a1a6141
hetame1 <121005861+hetame1@users.noreply.github.com>
2023-01-10 20:28:54 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,21 @@ var time = 30;
 
 $('a.showtimer').click(function() {
   $('div.timeShow').slideToggle("slow")
-  time = 0;
+  $("#buttonstart").text("START");
+  clearInterval(timer);
+  $("#time").text("00:00")
+  start = true;
 });
 
-
 $("#button1").click(function() {
+  if($(this).hasClass("starting")) {
+    clearInterval(timer);
+    time = 30;
+    start = true;
+  }
+  
   if(start) {
+    $("#button1").addClass("starting")
     time = 30;
     start = false;
   
@@ -25,6 +34,40 @@ $("#button1").click(function() {
 
     $("#time").html("00:"+time);
     time--;
+  }, 1000);
+}
+});
+
+$("#buttonstart").click(function() {
+  if($(this).hasClass("starting")) {
+    clearInterval(timer);
+    $("#buttonstart").text("START");
+    $("#buttonstart").removeClass("starting");
+    $("#time").text("00:00")
+    start = true;
+  }
+  else if(start) {
+    time = 0;
+    $("#buttonstart").text("RESET");
+    $("#buttonstart").addClass("starting");
+
+    start = false;
+  
+  timer = setInterval(function() {
+
+    if(time == 30) {
+      clearInterval(timer);
+      $("#buttonstart").text("START");
+      $("#buttonstart").css("width", "80px")
+      start = true;
+    }
+
+    if(time < 10) {
+      time = "0" + time;
+    }
+
+    $("#time").html("00:"+time);
+    time++;
   }, 1000);
 }
 });
